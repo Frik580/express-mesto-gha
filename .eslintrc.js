cbb9fee142f27487d1206b9b7c7d59124182f7c9
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2022-08-08 17:02:52 +0500

    Создание app.js и структуры проекта

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    es2021: true,
+  },
+  extends: ['eslint: recommended', 'plugin:react/recommended', 'airbnb-base'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: { "allow": ["_id"] },
+};
