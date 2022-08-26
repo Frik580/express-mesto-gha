12322bb787018662482f8aba8db86e76bc51418c
Arsenii Gerasimchuk <gerasimchuk_arseniy@yahoo.com>
2022-08-26 18:17:56 +0300

    На проверку 14 проектная работа

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const { createUser, login } = require('./contollers/users');
 const auth = require('./middlewares/auth');
 
 const app = express();
-// const { NotFound } = require('./errors/notfound');
 const { errorStatus } = require('./errors/errors');
 
 const { notFound } = errorStatus;
